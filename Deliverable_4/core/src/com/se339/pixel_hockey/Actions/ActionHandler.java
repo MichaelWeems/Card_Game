d27f2eccfcd9c562c10c6978a014a6321d7a7645
ztwild <ztwild@iastate.edu>
2016-11-30 23:03:22 -0600

    Revised

--- Deliverable_4/core/src/com/se339/pixel_hockey/Actions/ActionHandler.java
+++
@@ -1,8 +0,0 @@
-package com.se339.pixel_hockey.Actions;
-
-/**
- * Created by mweem_000 on 11/25/2016.
- */
-
-public class ActionHandler {
-}
--- Deliverable_4/core/src/com/se339/pixel_hockey/PixelHockeyGame.java
+++ Deliverable_4/core/src/com/se339/pixel_hockey/PixelHockeyGame.java
@@ -1,142 +1,31 @@
 package com.se339.pixel_hockey;
 
-import com.badlogic.gdx.ApplicationListener;
+/**
+ * Created by Zach on 11/30/2016.
+ */
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.input.GestureDetector;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Filter;
-import com.se339.pixel_hockey.Screens.PlayScreen;
 
+public class PixelHockeyGame extends Game {
 
-public class PixelHockeyGame extends Game implements ApplicationListener, GestureDetector.GestureListener {
-	//Virtual Screen size and Box2D Scale(Pixels Per Meter)
-	public static final int V_WIDTH = 400;
-	public static final int V_HEIGHT = 208;
-	public static final float PPM = 100;
+    SpriteBatch batch;
+    BitmapFont font;
 
-    private BitmapFont font;
-    private String message = "No gesture performed yet";
-    private int w,h;
+    public void create() {
+        batch = new SpriteBatch();
+        // Use LibGDX's default Arial font.
+        font = new BitmapFont();
+        this.setScreen(new MainMenuScreen(this));
+    }
 
-	//Box2D Collision Bits
-	public static final short NOTHING_BIT = 0;
-	public static final short GROUND_BIT = 1;
-	public static final short MARIO_BIT = 2;
-	public static final short BRICK_BIT = 4;
-	public static final short COIN_BIT = 8;
-	public static final short DESTROYED_BIT = 16;
-	public static final short OBJECT_BIT = 32;
-	public static final short ENEMY_BIT = 64;
-	public static final short ENEMY_HEAD_BIT = 128;
-	public static final short ITEM_BIT = 256;
-	public static final short MARIO_HEAD_BIT = 512;
+    public void render() {
+        super.render(); // important!
+    }
 
-	public SpriteBatch batch;
-
-	/* WARNING Using AssetManager in a static way can cause issues, especially on Android.
-	Instead you may want to pass around Assetmanager to those the classes that need it.
-	We will use it in the static context to save time for now. */
-	public static AssetManager manager;
-
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		manager = new AssetManager();
-		manager.load("audio/music/mario_music.ogg", Music.class);
-		manager.load("audio/sounds/coin.wav", Sound.class);
-		manager.load("audio/sounds/bump.wav", Sound.class);
-		manager.load("audio/sounds/breakblock.wav", Sound.class);
-		manager.load("audio/sounds/powerup_spawn.wav", Sound.class);
-		manager.load("audio/sounds/powerup.wav", Sound.class);
-		manager.load("audio/sounds/powerdown.wav", Sound.class);
-		manager.load("audio/sounds/stomp.wav", Sound.class);
-		manager.load("audio/sounds/mariodie.wav", Sound.class);
-
-		manager.finishLoading();
-
-		setScreen(new PlayScreen(this));
-
-        font = new BitmapFont(Gdx.files.internal("data/arial-15.fnt"),false);
-        font.setColor(Color.RED);
-        w = Gdx.graphics.getWidth();
-        h = Gdx.graphics.getHeight();
-
-        GestureDetector gd = new GestureDetector(this);
-        Gdx.input.setInputProcessor(gd);
-	}
-
-
-	@Override
-	public void dispose() {
-		super.dispose();
-		manager.dispose();
-		batch.dispose();
+    public void dispose() {
+        batch.dispose();
         font.dispose();
-	}
-
-	@Override
-	public void render () {
-		super.render();
-
-	}
-
-    @Override
-    public boolean touchDown(float x, float y, int pointer, int button) {
-        return false;
     }
 
-    @Override
-    public boolean tap(float x, float y, int count, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean longPress(float x, float y) {
-        return false;
-    }
-
-    @Override
-    public boolean fling(float velocityX, float velocityY, int button) {
-        if (this.getScreen() instanceof PlayScreen){
-            message = "Fling performed, velocity:" + Float.toString(velocityX) +
-                    "," + Float.toString(velocityY);
-            //Screen.newAction(fling)
-            System.out.println(message);
-
-
-        }
-        return false;
-    }
-
-    @Override
-    public boolean pan(float x, float y, float deltaX, float deltaY) {
-        return false;
-    }
-
-    @Override
-    public boolean panStop(float x, float y, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean zoom(float initialDistance, float distance) {
-        return false;
-    }
-
-    @Override
-    public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
-        return false;
-    }
-
-    @Override
-    public void pinchStop() {
-
-    }
 }
