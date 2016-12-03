b2d096eb4ee9e80a5adb5658b47e61bddad5d15f
ztwild <ztwild@iastate.edu>
2016-12-03 12:23:32 -0600

    working on friend screen and skins

--- Deliverable_4/core/src/com/se339/pixel_hockey/screens/FriendScreen.java
+++ Deliverable_4/core/src/com/se339/pixel_hockey/screens/FriendScreen.java
@@ -1,9 +1,16 @@
 package com.se339.pixel_hockey.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.se339.fileUtilities.FileList;
 import com.se339.log.Log;
 import com.se339.pixel_hockey.PixelHockeyGame;
 import com.se339.ui_elements.CSVReader;
@@ -24,12 +31,13 @@ import static com.se339.fileUtilities.FileList.test_csv;
 
 public class FriendScreen extends Screens {
     Stage stage;
+    Skin skin;
 
     public FriendScreen(PixelHockeyGame game) {
 
         super(game);
         Log log = new Log("Friend Screen");
-
+        skin = skin = new Skin();
 
         log.l("Creating Stage");
         stage = new Stage();
@@ -47,8 +55,18 @@ public class FriendScreen extends Screens {
 
 
         log.l("adding to Tile");
-        Table title = new Table();
+        Skin labelSkin = new Skin();
+        Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        log.a("Making pixmap and skins");
+        pixmap.setColor(Color.WHITE);
+        pixmap.fill();
+        labelSkin.add("white", new Texture(pixmap));
+        labelSkin.add("default", new BitmapFont());
 
+        Table title = new Table();
+        title.add(new Label("Friends", labelSkin));
+        title.row();
+        title.add(new Label("List", labelSkin));
         stage.addActor(title);
         title.setSize(stage.getWidth(), 120);
         title.setPosition(0, 0.8f);
--- Deliverable_4/core/src/com/se339/pixel_hockey/screens/MainMenuScreen.java
+++ Deliverable_4/core/src/com/se339/pixel_hockey/screens/MainMenuScreen.java
@@ -6,26 +6,12 @@ package com.se339.pixel_hockey.screens;
 
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import com.se339.fileUtilities.FileList;
 import com.se339.pixel_hockey.PixelHockeyGame;
-import com.se339.fileUtilities.FileList;
 import com.se339.log.Log;
 import com.se339.ui_elements.Hud;
-import com.se339.ui_elements.buttonIcon;
 
 public class MainMenuScreen extends Screens {
     Stage stage;
@@ -65,20 +51,7 @@ public class MainMenuScreen extends Screens {
 
 
     }
-//    public void setFriendScreen(){
-//        game.setScreen(new GameScreen(game));
-//        dispose();
-//    }
-//
-//    public void setStatScreen(){
-//        game.setScreen(new GameScreen(game));
-//        dispose();
-//    }
-//
-//    public void findGame(){
-//        game.setScreen(new GameScreen(game));
-//        dispose();
-//    }
+
 
     @Override
     public void resize(int width, int height) {
