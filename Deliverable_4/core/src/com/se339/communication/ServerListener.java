fcdf298076f595f0296943aded4e43f192acb8ba
ztwild <ztwild@iastate.edu>
2016-12-05 23:48:56 -0600

    serverlistener

--- Deliverable_4/core/src/com/se339/communication/ServerListener.java
+++ Deliverable_4/core/src/com/se339/communication/ServerListener.java
@@ -1,10 +1,12 @@
 package com.se339.communication;
 
+import com.badlogic.gdx.math.Vector2;
 import com.se339.log.Log;
 import com.se339.pixel_hockey.PixelHockeyGame;
 import com.se339.pixel_hockey.screens.GameScreen;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Created by mweem_000 on 12/5/2016.
@@ -32,9 +34,17 @@ public class ServerListener {
                             game.setScreen(new GameScreen(game));
                         }
                         else if (msg.equals("goal")){
-
+                            //add point to player
+                        }
+                        else if(msg.contains("name")){
+                            String opp = game.getSocket().read();
+                            //save opp name
                         }
                         else if (msg.contains("velocity")){
+                            Scanner scan = new Scanner(game.getSocket().read());
+                            float x = scan.nextInt();
+                            float y = scan.nextInt();
+                            Vector2 v = new Vector2(x,y);
 
                         }
 
